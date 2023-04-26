8f747ab8d505bf7e9c3ebc76b7a73e87dbfbc598
Leonel Chauca <leonelchauca0@gmail.com>
2023-04-25 23:30:41 -0400

    primeros pasos con material ui

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,19 +3,7 @@ import React from 'react'
 export const Login = () => {
   return (
     <>
-        <div className=' '>
-            <div className=' '>
-                <h3 className=' '>INICIAR SESION</h3>
-                <form action="" className=''>
-                    <input type="text" className=''  />
-                    <br />
-                    <input type="text" className=''/>
-                    <br />
-                    <button>Enviar</button>
-                </form>
-                <p>Lorem ipsum dolor, sit</p>
-            </div>      
-        </div>
+        
     </>
   )
 }
