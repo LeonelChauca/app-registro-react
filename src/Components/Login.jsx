a6141913f74879ceeb3991146323ef5016113721
Leonel Chauca <leonelchauca0@gmail.com>
2023-04-25 22:40:02 -0400

    cambiando a material ui
    eliminando a tailwind

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export const Login = () => {
   return (
     <>
-        <div className=' h-full-login flex items-center justify-center'>
-            <div className=' bg-blue-300 p-4 flex flex-col items-center w-2/6 h-4/6'>
-                <h3 className=' text-3xl py-4'>INICIAR SESION</h3>
-                <form action="" className='w-full'>
-                    <input type="text" className='form-input w-full'  />
+        <div className=' '>
+            <div className=' '>
+                <h3 className=' '>INICIAR SESION</h3>
+                <form action="" className=''>
+                    <input type="text" className=''  />
                     <br />
-                    <input type="text" className='form-input w-full'/>
+                    <input type="text" className=''/>
                     <br />
                     <button>Enviar</button>
                 </form>
